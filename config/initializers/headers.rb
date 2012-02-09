509d59d1a6db676b9bad5627a15fb872eeca626b
Jo Liss <joliss42@gmail.com>
2012-02-09 04:26:53 +0100

    Disable caching in development and test

--- config/initializers/headers.rb
+++ config/initializers/headers.rb
@@ -8,14 +8,29 @@ module Solitr
       def call(env)
         status, headers, body = @app.call(env)
 
-        case env['REQUEST_PATH']
-        when %r{^/assets/.*-[0-9a-f]{32,}}
-          duration = 1.month
-        else
-          duration = 1.hour
+        duration = nil
+        if Rails.env.production?
+          case env['REQUEST_PATH']
+          when %r{^/assets/.*-[0-9a-f]{32,}}
+            duration = 1.month
+          else
+            duration = 1.hour
+          end
         end
-        headers['Expires'] = "#{duration.from_now}"
-        headers['Cache-Control'] = "public, max-age=#{duration.to_i}"
+
+        if duration
+          headers['Expires'] = "#{duration.from_now}"
+          headers['Cache-Control'] = "public, max-age=#{duration.to_i}"
+        else
+          headers['Pragma'] = 'no-cache'
+          headers['Cache-Control'] = 'no-cache'
+          headers['Expires'] = '-1'
+          # Sometimes ETag and Last-Modified headers are messed up by Sprockets
+          if Rails.env.development?
+            headers.delete 'ETag'
+            headers.delete 'Last-Modified'
+          end
+        end
 
         [status, headers, body]
       end
